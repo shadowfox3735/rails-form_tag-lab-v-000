a36e985fabae1c509ec0de70eec2e68f67be4901
shadowfox3735 <normanpan180@gmail.com>
2020-02-07 20:49:47 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,8 @@ class StudentsController < ApplicationController
   end
   
   def create
-    
+    Post.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
   end
 
 end
